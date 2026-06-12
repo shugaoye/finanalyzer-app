febd235369271c49fbe427f104055459447ce830
Roger Ye <shugaoye@yahoo.com>
2026-06-12 11:30:46 +0800

    release: bump version to 1.14.0
    将项目版本从开发版1.14.0-dev正式更新为1.14.0，同步更新中英文国际化文件中的版本号展示

--- src/i18n/resources/zh.ts
+++ src/i18n/resources/zh.ts
@@ -231,7 +231,7 @@ export default {
       english: "English",
       chinese: "中文",
     },
-    version: "v1.14.0-dev", // Replace with the actual version number
+    version: "v1.14.0", // Replace with the actual version number
   },
   mobileNav: {
     menu: "菜单",
